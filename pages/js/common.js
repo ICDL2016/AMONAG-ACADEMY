8213a2b1c6c77db6be0108c69d7bbda9f7cf3eb7
User <firuz.jobcontact@yandex.ru>
2016-11-02 19:39:14 +0300

    Version 1.4

--- pages/js/common.js
+++ pages/js/common.js
@@ -72,6 +72,16 @@ $(document).ready(function () {
 		$(".lang__list").slideToggle();
 	});
 
+
+	$(".language").click(function(){
+		$(".sidebar__nav").slideUp();
+	});
+
+	$(".burger").click(function(){
+		$(".lang__list").slideUp();
+	});
+
+
 	$( ".mobile__search_btn" ).click(function() {
 		$( ".search__block input[type='text']" ).fadeIn( 300, "linear" );
 	});
@@ -82,7 +92,37 @@ $(document).ready(function () {
 });
 
 
+(function($){
+	$(function() {
 
+		$('.jQtooltip').each(function() {
+			var el = $(this);
+			var title = el.attr('title');
+			if (title && title != '') {
+				el.attr('title', '').append('<div>' + title + '</div>');
+				var width = el.find('div').width();
+				var height = el.find('div').height();
+				el.hover(
+					function() {
+						el.find('div')
+							.clearQueue()
+							.delay(200)
+							.animate({width: width + 20, height: height + 20}, 200).show(200)
+							.animate({width: width, height: height}, 200);
+					},
+					function() {
+						el.find('div')
+							.animate({width: width + 20, height: height + 20}, 150)
+							.animate({width: 'hide', height: 'hide'}, 150);
+					}
+				).mouseleave(function() {
+					if (el.children().is(':hidden')) el.find('div').clearQueue();
+				});
+			}
+		})
+
+	})
+})(jQuery)
 
 
 
