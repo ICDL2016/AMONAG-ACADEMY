6b04d3df7791f67871a6d7f9413c384bf04d14e1
User <firuz.jobcontact@yandex.ru>
2016-11-06 13:52:15 +0300

    Version 2.6

--- pages/js/common.js
+++ pages/js/common.js
@@ -137,7 +137,28 @@ $(document).ready(function () {
 
 
 
+$(document).ready(function(){
+	var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i; //name-_09@mail09-.ru
+	var mail = $('#mail');
 
+	mail.blur(function(){
+		if(mail.val() != ''){
+			if(mail.val().search(pattern) == 0){
+				$('#valid').text('Подходит');
+				$('#submit').attr('disabled', false);
+				mail.removeClass('error').addClass('ok');
+			}else{
+				$('#valid').text('Не подходит');
+				$('#submit').attr('disabled', true);
+				mail.addClass('error');
+			}
+		}else{
+			$('#valid').text('Поле e-mail не должно быть пустым!');
+			mail.addClass('error');
+			$('#submit').attr('disabled', true);
+		}
+	});
+});	
 
 
 
