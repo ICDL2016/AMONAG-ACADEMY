b23761fe65bdbad3b711bb98cdd870b9a56a6698
timmyru <33581137+timmyru@users.noreply.github.com>
2019-08-28 19:31:04 +0300

    Add files via upload

---
+++ amonag/iab-pages/_header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="ru"> <!--<![endif]-->
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title>Новый ЛК</title>
+    <meta name="description" content="">
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="img/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="img/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="img/favicons/manifest.json">
+    <link rel="mask-icon" href="img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#800000">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes">
+
+
+    <link rel="stylesheet" href="libs/animate/animate.css">
+
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/cssnbusu.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/media.css">
+
+    <script src="libs/modernizr/modernizr.js"></script>
+    <link rel="stylesheet" type="text/css" href="source/jquery.fancybox.css?v=2.1.5" media="screen" />
+
+</head>
+
+<body class="bg__white">
+<header class="header">
+    <div class="inner__content">
+        <div class="logo__block jQtooltip" title="Главная страница">
+            <a href="index.html"><img src="img/logo.png" alt=""></a>
+        </div>
+
+
+        <ul class="nav__btn">
+            <!--<li>-->
+            <!--<span class="language jQtooltip" title="Сменить язык сайта">Ru</span>-->
+            <!--<ul class="lang__list">-->
+            <!--<li><a href="#">EN</a></li>-->
+            <!--<li><a href="#">ES</a></li>-->
+            <!--<li><a href="#">DE</a></li>-->
+            <!--<li><a href="#">FR</a></li>-->
+            <!--<li><a href="#">IT</a></li>-->
+            <!--<li><a href="#">PT</a></li>-->
+            <!--</ul>-->
+            <!--</li>-->
+            <li class="jQtooltip" title="Вход">
+                <a class="icon__enter" href="#"><i class="fa fa-graduation-cap"></i></a>
+            </li>
+            <li>
+					<span class="burger jQtooltip" title="Меню">
+						<span></span>
+						<span></span>
+						<span></span>
+					</span>
+            </li>
+        </ul>
+        <div class="search__block">
+            <div class="site__title">
+                <a href="#">IAB</a>
+            </div>
+            <!--<div class="input__pole">-->
+            <!--<input type="text" placeholder="Найти курс обучения в мультиязычном формате"/>-->
+            <!--</div>-->
+            <!--<div class="search__btn_block">-->
+            <!--<a class="search__btn desktop__search_btn jQtooltip" title="Искать" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>-->
+            <!--<a class="search__btn mobile__search_btn" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>-->
+            <!--</div>-->
+        </div>
+        <div class="clear"></div>
+    </div>
+</header>
+<div class="sidebar__nav">
+    <ul>
+        <li><a href="#">Все курсы</a></li>
+        <li><a href="#">Языковые курсы</a></li>
+        <li><a href="#">Маркетинг и реклама</a></li>
+        <li><a href="#">Экономика</a></li>
+        <li><a href="#">Дизайн</a></li>
+        <li><a href="#">Мода</a></li>
+        <li><a href="#">Психология</a></li>
+        <li><a href="#">Ресторанный бизнес</a></li>
+        <li><a href="#">Гостиничный бизнес</a></li>
+        <li><a href="#">Журналистика и массовые коммуникации</a></li>
+        <li><a href="#">AMONAG ACADEMY</a></li>
+        <li><a href="#">Блог</a></li>
+        <li><a href="#">Вопросы и ответы</a></li>
+        <li><a href="#">Контакты</a></li>
+    </ul>
+</div>
